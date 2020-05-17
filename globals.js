283b1248b9e52dd82381a15e288885e3c3cf038f
alanrempel95 <alanrempel95@gmail.com>
2020-05-17 09:22:39 -0400

    Update globals.js

--- globals.js
+++ globals.js
@@ -1,10 +1,3 @@
-//template for people - @Jordan feel free to change
-var personTemp = {
-    personRadius: 15,
-    personX: 300,
-    personY: 300
-};
-
 //global object
 var groceryMap = {
     tileSize: 30,
